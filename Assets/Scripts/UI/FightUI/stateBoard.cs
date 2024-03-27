ab9af2f8edd6f6ae221268ff210bafcb23b54bbb
F LIU <1732967400@qq.com>
2024-03-27 15:53:10 +0800

    优化了循序面板血条，制作完成了具体信息面板

--- Assets/Scripts/UI/FightUI/stateBoard.cs
+++ Assets/Scripts/UI/FightUI/stateBoard.cs
@@ -22,10 +22,32 @@ public class stateBoard:MonoBehaviour,IPointerClickHandler
     public UnityEngine.UI.Image healthImage;
     public GameObject turnTip;
 
+    private TextMeshProUGUI HealthText;
+    private TextMeshProUGUI BarrierText;
+    private TextMeshProUGUI MobilityText;
+    private TextMeshProUGUI MoveModeText;
+    private TextMeshProUGUI MeleeAttackText;
+    
+
+
 
     private void Awake()
     {
-    
+        detailedPanel = GameObject.Find("detailedStatePanel");
+        chessName=GameObject.Find("chessName").GetComponent<TextMeshProUGUI>();
+        CardDescription = GameObject.Find("CardDescription").GetComponent<TextMeshProUGUI>();
+        detailedState = GameObject.Find("detailedState").GetComponent<TextMeshProUGUI>();
+
+        //从detailedPanel中获取组件
+        HealthText = GameObject.Find("HealthText").GetComponent<TextMeshProUGUI>();
+        BarrierText = GameObject.Find("BarrierText").GetComponent<TextMeshProUGUI>();
+        MobilityText = GameObject.Find("MobilityText").GetComponent<TextMeshProUGUI>();
+        MoveModeText = GameObject.Find("MoveModeText").GetComponent<TextMeshProUGUI>();
+        MeleeAttackText = GameObject.Find("MeleeAttackText").GetComponent<TextMeshProUGUI>();
+
+        tooltipPanel = GameObject.Find("EntryExplanation");
+        EntryNameText = GameObject.Find("EntryNameText").GetComponent<TextMeshProUGUI>();
+        EntryExplanationText = GameObject.Find("EntryExplanationText").GetComponent<TextMeshProUGUI>();
     }
     private void Update()
     {
@@ -48,12 +70,98 @@ public class stateBoard:MonoBehaviour,IPointerClickHandler
         //    }
         //    if(!enemy.isActed && turnTip.GetComponent<CanvasGroup>().alpha==1) { turnTip.GetComponent<CanvasGroup>().DOFade(0, 0.2f); }
         //}
+
+        if (isClicked)
+        {
+            chessName.text = PlayerController.instance.name;
+            CardDescription.text = description;
+            HealthText.text = "currHealth: " + PlayerController.instance.Hp;
+            BarrierText.text = "Barrier: " + PlayerController.instance.Barrier;
+            MobilityText.text = "Mobility: --";
+            MoveModeText.text = "MoveMode: --";
+            MeleeAttackText.text = "MeleeAttack: " + PlayerController.instance.MeleeAttackPower;
+        }
+
+        if (isTooltipActive)
+        {
+            tooltipPanel.transform.position = Input.mousePosition;
+        }
     }
 
+    bool isClicked = false;
     public void OnPointerClick(PointerEventData eventData)
     {
         //detailedPanel.SetActive(!detailedPanel.activeSelf);
-        if (detailedPanel.GetComponent<CanvasGroup>().alpha == 0) detailedPanel.GetComponent<CanvasGroup>().alpha = 1; else detailedPanel.GetComponent<CanvasGroup>().alpha = 0;
+        if (detailedPanel.GetComponent<CanvasGroup>().alpha == 0) 
+        {
+            detailedPanel.GetComponent<CanvasGroup>().alpha = 1;
+            isClicked = true;
+        } 
+        else 
+        {
+            detailedPanel.GetComponent<CanvasGroup>().alpha = 0;
+            isClicked = false;
+        }
         detailedPanel.GetComponent<CanvasGroup>().DOFade(detailedPanel.GetComponent<CanvasGroup>().alpha, 0.2f);
+        //Debug.Log(thisEnemy.name+" + "+ thisEnemy.HP);
     }
+
+    
+
+
+    //用于词条解释的方法
+    public GameObject tooltipPanel; // 面板
+    public TextMeshProUGUI EntryNameText; // 文本
+    public TextMeshProUGUI EntryExplanationText; // 文本
+    private bool isTooltipActive = false;
+    public void ShowTooltip(string tooltipString, string tooltipName)
+    {
+        // 设置面板的位置和文本
+        isTooltipActive = true;
+        EntryNameText.text = tooltipName;
+        EntryExplanationText.text = tooltipString;
+        tooltipPanel.GetComponent<CanvasGroup>().alpha = 1;
+    }
+
+    public void HideTooltip()
+    {
+        tooltipPanel.GetComponent<CanvasGroup>().alpha = 0;
+        isTooltipActive = false;
+    }
+
+    public void OnHealthEnter()
+    {
+        Debug.Log("Health");
+        if (detailedPanel.GetComponent<CanvasGroup>().alpha == 1)
+        {ShowTooltip("This is the detailed explanation of health", "Health");}
+    }
+
+    public void OnBarrierEnter()
+    {
+        Debug.Log("Barrier");
+        if (detailedPanel.GetComponent<CanvasGroup>().alpha == 1)
+        {ShowTooltip("This is the detailed explanation of barrier", "Barrier");}
+    }
+
+    public void OnMobilityEnter()
+    {
+        Debug.Log("Mobility");
+        if (detailedPanel.GetComponent<CanvasGroup>().alpha == 1)
+        {ShowTooltip("This is the detailed explanation of mobility", "Mobility");}
+    }
+
+    public void OnMoveModeEnter()
+    {
+        Debug.Log("Move Mode");
+        if (detailedPanel.GetComponent<CanvasGroup>().alpha == 1)
+        {ShowTooltip("This is the detailed explanation of move mode", "Move Mode");}
+    }
+
+    public void OnMeleeAttackEnter()
+    {
+        Debug.Log("Melee Attack");
+        if (detailedPanel.GetComponent<CanvasGroup>().alpha == 1) ShowTooltip("This is the detailed explanation of melee attack", "Melee Attack");
+    }
+
+
 }
