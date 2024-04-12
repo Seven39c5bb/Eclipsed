59b7907646e9d0d1dea2fa5a0e6b2911a5c025ec
F LIU <1732967400@qq.com>
2024-04-12 16:06:45 +0800

    更新了光标素材

--- Assets/Scripts/Atlas/CameraController.cs
+++ Assets/Scripts/Atlas/CameraController.cs
@@ -19,9 +19,17 @@ public class CameraController : MonoBehaviour
     {
         if (Input.GetMouseButton(1))
         {
+            Texture2D cursorTex = Resources.Load("Cursors/Cursors 256/Cursor_Hand") as Texture2D;
+            Cursor.SetCursor(cursorTex, Vector2.zero, CursorMode.Auto);
+
             Vector3 mouseDelta = new Vector3(-Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y"), 0);
             float dampingFactor = 0.3f; // 阻尼系数，你可以根据需要调整
             this.gameObject.transform.Translate(mouseDelta * dampingFactor);
+        }
+        else
+        {
+            Texture2D cursorTex = Resources.Load("Cursors/Cursors 256/Cursor_Basic2") as Texture2D;
+            Cursor.SetCursor(cursorTex, Vector2.zero, CursorMode.Auto);
         }
         
     }
