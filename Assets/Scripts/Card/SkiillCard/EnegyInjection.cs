6a886bc37f17ae7741e3aa823e653b6c51ac86bb
Seven39c5bb <130481796+Seven39c5bb@users.noreply.github.com>
2024-04-25 16:59:19 +0800

    修改了部分字体和注能卡牌bug

--- Assets/Scripts/Card/SkiillCard/EnegyInjection.cs
+++ Assets/Scripts/Card/SkiillCard/EnegyInjection.cs
@@ -7,6 +7,6 @@ public class EnegyInjection : Card
     public override void CardFunc()
     {
         //添加一个注能buff
-        BuffManager.instance.AddBuff("EnergyInjectionBuff", PlayerController.instance);
+        BuffManager.instance.AddBuff("EnegyInjectionBuff", PlayerController.instance);
     }
 }
